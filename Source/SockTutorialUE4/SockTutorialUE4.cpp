fb30845749f240b4a22caab6924a90b582d9be15
odesai840 <odesai840@gmail.com>
2026-05-05 23:43:05 -0400

    project source

---
+++ Source/SockTutorialUE4/SockTutorialUE4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SockTutorialUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SockTutorialUE4, "SockTutorialUE4" );
---
+++ Source/SockTutorialUE4/SockTutorialUE4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SockTutorialUE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOCKTUTORIALUE4_API ASockTutorialUE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
